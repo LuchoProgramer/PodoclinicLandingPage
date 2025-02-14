18c9e60766425caca9019405c4b5bcd8d92e69c1
LuchoProgramer <luchoviteri1990@gmail.com>
2025-02-13 22:32:04 -0500

    Palabras clave

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,17 @@
+// app/layout.js
 import "../styles/globals.css";
 
 export const metadata = {
   title: "Podología en Quito | Tratamiento para Pie Diabético y Uñeros",
   description: "Atención podológica especializada en Quito. Tratamiento de uñeros, pie diabético, onicomicrosis y verrugas. Reserva tu cita hoy.",
   robots: "index, follow",
+  keywords: [
+    "podología",
+    "pie diabético",
+    "uñeros",
+    "tratamiento uñeros",
+    "podología en Quito"
+  ],
   openGraph: {
     title: "Podología en Quito | Tratamiento para Pie Diabético y Uñeros",
     description: "Atención podológica especializada en Quito. Trato personalizado para adultos mayores y pacientes con pie diabético.",
@@ -24,7 +32,9 @@ export const metadata = {
     card: "summary_large_image",
     title: "Podología en Quito | Tratamiento para Pie Diabético y Uñeros",
     description: "Atención podológica especializada en Quito. Trato personalizado para adultos mayores y pacientes con pie diabético.",
-    images: ["https://res.cloudinary.com/dbbukhtz5/image/upload/v1739392953/PODOCLINIC_LOGO_uerq9h.png"],
+    images: [
+      "https://res.cloudinary.com/dbbukhtz5/image/upload/v1739392953/PODOCLINIC_LOGO_uerq9h.png"
+    ],
   },
 };
 
