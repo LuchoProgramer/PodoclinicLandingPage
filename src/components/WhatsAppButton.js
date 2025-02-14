a58aee43485b12d2fdb7467b752256ad72028864
LuchoProgramer <luchoviteri1990@gmail.com>
2025-02-14 16:31:33 -0500

    whatsapp buttom

--- src/components/WhatsAppButton.js
+++ src/components/WhatsAppButton.js
@@ -12,7 +12,7 @@ export default function WhatsAppButton() {
             href={whatsappLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="fixed bottom-6 right-6 bg-[#25D366] text-white p-4 rounded-full shadow-lg hover:bg-[#1EBE5D] transition duration-300 flex items-center justify-center"
+            className="fixed bottom-6 right-6 bg-[#60BEC3] text-white p-4 rounded-full shadow-lg hover:bg-[#1EBE5D] transition duration-300 flex items-center justify-center"
         >
             <FaWhatsapp className="w-8 h-8" /> {/* ✅ Ícono más moderno y reconocible */}
         </a>
