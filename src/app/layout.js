6efe7a1a7892ccbc3119864d71da6dedb5a68175
LuchoProgramer <luchoviteri1990@gmail.com>
2025-10-01 18:05:08 -0500

    Agregar pixel de Facebook

--- src/app/layout.js
+++ src/app/layout.js
@@ -69,6 +69,30 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-FHN5JGKH34');
           `}
         </Script>
+        
+        {/* Facebook Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '827077656519595');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=827077656519595&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className="flex flex-col min-h-screen">
         {/* Google Tag Manager (noscript) */}
