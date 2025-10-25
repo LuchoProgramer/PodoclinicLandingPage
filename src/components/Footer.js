6aa1e023032824e03f27832ee6dde9c0a8c874f7
LuchoProgramer <luchoviteri1990@gmail.com>
2025-10-24 23:19:13 -0500

    telefono

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,9 @@ export function Footer() {
     return (
         <footer className="bg-gray-800 text-white p-6 text-center">
             <p>&copy; 2025 PodoClinic. Todos los derechos reservados.</p>
+            <p className="mt-2">
+                Contacto: <a href="tel:+593995832788" className="underline hover:text-[#60BEC3]">+593 995 832 788</a>
+            </p>
         </footer>
     );
 }
